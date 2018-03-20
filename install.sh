25549cc9b8e3baefecdc385199cda48cf190baa1
Martijn Steenbergen (on Samaritan) <martijnsreclame@hotmail.nl>
2018-03-20 11:46:59 +0100

    Fix some errors

--- install.sh
+++ install.sh
@@ -1,17 +1,35 @@
-sudo apt-get install zsh dotnet htop maven
-chsh -s $(which zsh)
+#bin/bash
 
-# Linking all files
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/vscode/keybindings.json ~/.config/Code/User/
-ln -s ~/.dotfiles/vscode/settings.json ~/.config/Code/User/
-ln -s ~/.dotfiles/.nanorc ~/.nanorc
-ln -s ~/.dotfiles/.bashrc ~/.bashrc
-ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
-ln -s ~/.dotfiles/.dir_colors ~/.dir_colors
+# dotnet
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg 
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg 
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get install apt-transport-https
+sudo apt-get update
+
+#Install
+sudo apt-get install zsh dotnet-sdk-2.1.101 htop maven 
+
+chsh -s $(which zsh)
 
 # Install oh my zsh
 export ZSH="$HOME/.oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zdharma/fast-syntax-highlighting.git $ZSH_CUSTOM/plugins/fast-syntax-highlighting
+# Linking all files
+function link {
+	rm $2
+	ln -s $1 $2
+}
+
+
+link ~/.dotfiles/.zshrc ~/.zshrc
+link ~/.dotfiles/vscode/keybindings.json ~/.config/Code/User/
+link ~/.dotfiles/vscode/settings.json ~/.config/Code/User/
+link ~/.dotfiles/.nanorc ~/.nanorc
+link ~/.dotfiles/.bashrc ~/.bashrc
+link ~/.dotfiles/.bash_aliases ~/.bash_aliases
+link ~/.dotfiles/.dir_colors ~/.dir_colors
+
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zdharma/fast-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/fast-syntax-highlighting
